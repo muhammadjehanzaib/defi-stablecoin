91aff436a04bf6422aecf4d7826de35bde0b7d3a
muhammadjehanzaib <soulsniper155@gmail.com>
2024-11-28 11:08:49 +0500

    Reset balanceOf arrguments from weth to dscEngine

--- test/fuzz/InvariantsTest.t.sol
+++ test/fuzz/InvariantsTest.t.sol
@@ -1,13 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
 
 contract InvariantTest is StdInvariant, Test {
     DSCEngine public dscEngine;
@@ -16,21 +17,30 @@ contract InvariantTest is StdInvariant, Test {
     DeployDSC public deploy;
     address weth;
     address wbtc;
+    Handler handler;
 
     function setUp() external {
         deploy = new DeployDSC();
         (dsc, dscEngine, config) = deploy.run();
         (,, weth, wbtc,) = config.activeNetworkConfig();
-        targetContract(address(dscEngine));
+        // targetContract(address(dscEngine));
+        handler = new Handler(dscEngine, dsc);
+        targetContract(address(handler));
     }
 
     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
         uint256 totalSupply = dsc.totalSupply();
         uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dscEngine));
-        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(wbtc));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dscEngine));
 
         uint256 wethValue = dscEngine.getUsdValue(weth, totalWethDeposited);
         uint256 wbtcValue = dscEngine.getUsdValue(wbtc, totalBtcDeposited);
+
+        console.log("weth value", wethValue);
+        console.log("wbtc value", wbtcValue);
+        console.log("total supply", totalSupply);
+        console.log("Times mint called", handler.timesMintIsCalled());
+        console.log("Times Deposit Callateral called", handler.timeDepositCollateralCalled());
 
         assert(wbtcValue + wethValue >= totalSupply);
     }
